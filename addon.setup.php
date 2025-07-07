e4f55eaae67d6872711c8a033b929ac78bb2d32a
Bryan Nielsen <bryan@packettide.com>
2025-07-07 13:46:09 -0400

    Update dependencies for PHP 8.4 support

--- addon.setup.php
+++ addon.setup.php
@@ -1,4 +1,8 @@
 <?php
+// Cloud Files works with all PHP versions supported by ExpressionEngine 7
+// However the S3 SDK is now showing warnings when used with older PHP versions
+// https://aws.amazon.com/blogs/developer/announcing-the-end-of-support-for-php-runtimes-8-0-x-and-below-in-the-aws-sdk-for-php/
+$_ENV['AWS_SUPPRESS_PHP_DEPRECATION_WARNING'] = $_ENV['AWS_SUPPRESS_PHP_DEPRECATION_WARNING'] ?? true;
 
 require __DIR__ . '/vendor-build/autoload.php';
 
